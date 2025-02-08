ee7ca68653fc5f41654c21b36770af7a0802f8dd
Liem Phan <z5419546@student.unsw.edu.au>
2025-02-09 01:33:29 +1100

    Fixed negative issue.

--- backend.js
+++ backend.js
@@ -14,7 +14,7 @@ function multiply(x, y) {
     return x * y;
 }
 
-function operate(operator, x, y) {
+function operate(operator, x, y) {      
     if (operator == "x") {
         return multiply(x, y);
     } else if (operator == "/") {
@@ -26,28 +26,11 @@ function operate(operator, x, y) {
     }
 }
 
-function splitOperation(text) {
+function splitOperation(text, operationIndex) {
     text = text.trim();
-
-    let question;
-    
-    if (text.includes("/")) {
-        question = text.split("/");
-        question.splice(2, 0, "/");
-    } else if (text.includes("x")) {
-        question = text.split("x");
-        question.splice(2, 0, "x");
-    } else if (text.includes("+")) {
-        question = text.split("+");
-        question.splice(2, 0, "+");
-    } else if (text.includes("-")) {
-        question = text.split("-");
-        question.splice(2, 0, "-");
-    }
-
-    question[0] = question[0].trim().split(" ").join("");
-    question[1] = question[1].trim().split(" ").join("");
-    question[2] = question[2].trim().split(" ").join("");
+    let question = []
+    question[0] = text.trim().split(" ").join("").slice(0, operationIndex);
+    question[1] = text.trim().split(" ").join("").slice(operationIndex + 1);
     
     return question;
 }
@@ -57,6 +40,7 @@ let result;
 let x;
 let y;
 let operation; 
+let operationIndex;
 
 let numButtons = document.querySelectorAll("#numberButton");
 let equalButton = document.querySelector("#equalButton");
@@ -72,30 +56,41 @@ numButtons.forEach((value) => {
 })
 
 operationButton.forEach((value) => {
-    value.addEventListener("click", (event) => {
+    value.addEventListener("click", (event) => {        
         let addedOperator = event.target.textContent;
 
-        if ((questionScreen.textContent.includes("+") || questionScreen.textContent.includes("-") || questionScreen.textContent.includes("x") || questionScreen.textContent.includes("/") && questionScreen)) {
-            questions = splitOperation(questionScreen.textContent);
-            questionScreen.textContent = "";
-            x = parseInt(questions[0]);
-            y = parseInt(questions[1]);
-            operation = questions[2];
+        if (questionScreen.textContent.trim().split(" ").join("") == "") {
+            questionScreen.textContent = questionScreen.textContent.trim() + addedOperator;
+        } else if (! isNaN(Number(questionScreen.textContent.trim().split(" ").join("")))) {
+            operation = addedOperator.trim().split(" ").join(""); 
+            operationIndex = questionScreen.textContent.trim().split(" ").join("").length;
 
-            questionScreen.textContent = operate(operation, x, y) + " "  + addedOperator;
+            console.log(operationIndex);
+            questionScreen.textContent = questionScreen.textContent + operation;
         } else {
-            answerScreen.textContent = "";
-            questionScreen.textContent = questionScreen.textContent + value.textContent;
+            questions = splitOperation(questionScreen.textContent + addedOperator, operationIndex);
+
+            if ((isNaN(Number(questions[1])) && (! isNaN(parseInt(questions[1]))))) {
+                x = parseInt(questions[0]);
+                y = parseInt(questions[1]);
+    
+                result = operate(operation, x, y);
+    
+                operation = addedOperator.trim().split(" ").join("");
+                questionScreen.textContent = result + operation;
+            } else {
+                questionScreen.textContent = questionScreen.textContent + addedOperator.trim().split(" ").join("");
+            }
         }
     })
 })
 
 equalButton.addEventListener("click", () => {
-    questions = splitOperation(questionScreen.textContent);
+    questions = splitOperation(questionScreen.textContent, operationIndex);
+
     questionScreen.textContent = "";
     x = parseInt(questions[0]);
     y = parseInt(questions[1]);
-    operation = questions[2];
 
     answerScreen.textContent = operate(operation, x, y);
 })
